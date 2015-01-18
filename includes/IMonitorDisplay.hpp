f14685df78784322cedc9c43618c023a62001bc2
Tristan Valentim <tvalentim@localhost.localdomain>
2015-01-18 08:46:03 +0100

    Added interfaces for modules and displays

--- includes/IMonitorDisplay.hpp
+++ includes/IMonitorDisplay.hpp
@@ -6,7 +6,7 @@
 /*   By: abarbaro <abarbaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/18 08:37:59 by abarbaro          #+#    #+#             */
-/*   Updated: 2015/01/18 08:42:43 by abarbaro         ###   ########.fr       */
+/*   Updated: 2015/01/18 08:45:51 by abarbaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,13 @@ class IMonitorDisplay
 public:
 	virtual ~IMonitorDisplay();
 
-	virtual void		clear();
-	virtual void		close();
-	virtual void		addModule();
-	virtual void		delModule();
+	virtual void		clear() = 0;
+	virtual void		close() = 0;
+	virtual void		addModule() = 0;
+	virtual std::string	listModules() = 0;
+	virtual void		delModule() = 0;
 };
+
+std::ostream & operator << (std::ostream & out, IMonitorDisplay ID);
 
 #endif
